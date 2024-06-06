c3d54505301d96300e0b81eb2849e5ccb8f6bb00
Hyoso0w0 <163081966+Hyoso0w0@users.noreply.github.com>
2024-06-07 00:28:01 +0900

    일단 달력 자체 구현완료

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,13 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useSession, SessionProvider } from "next-auth/react";
-import Diary from "@/components/Diary";
+import Calendar from '@/components/Calendartmp';
 import ChatPage from '@/components/ChatPage';
-
+import Diary from '@/components/Diary';
 import "../styles/globals.css";
 
 const Main = () => {
-  const router = typeof window !== 'undefined' ? useRouter() : null;
-
- 
-
-
+  const router = useRouter();
   const { data: session, status } = useSession({
     required: true,
     onUnauthenticated() {
@@ -26,16 +22,25 @@ const Main = () => {
     if (!router) return;
   }, [router]);
 
-  return <Diary user={session?.user} />;
-};
+  const handleDateClick = (day) => {
+    // 달력에서 날짜를 클릭할 때 다이어리 페이지로 이동
+    router.push({
+      pathname: '/diary',
+      query: { selectedDate: day }, // 선택한 날짜를 쿼리 파라미터로 전달
+    });
+  };
 
-const MainWithSession = ({ date }) => {
   return (
-    <SessionProvider>
-      <Main/>
-    </SessionProvider>
+    <div className="main-container flex flex-row">
+      <div className="chat-container w-1/3 p-4">
+        <ChatPage user={session?.user} />
+      </div>
+      <div className="calendar-container w-2/3 p-4" >
+        {/* Calendar에 handleDateClick 함수 전달 */}
+        <Calendar user={session?.user} onDateClick={handleDateClick} />
+      </div>
+    </div>
   );
 };
 
 export default Main;
-
